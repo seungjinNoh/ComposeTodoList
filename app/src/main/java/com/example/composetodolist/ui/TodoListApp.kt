82fae7ee7881ba7d638673de63ac79e59e646be0
seungjin <seungjin3047@gmail.com>
2024-04-04 23:05:35 +0900

    feat: FloatingActionButton Navigation 연결
    - 버튼 이미지
    - 클릭 시 EditScreen 이동

--- app/src/main/java/com/example/composetodolist/ui/TodoListApp.kt
+++ app/src/main/java/com/example/composetodolist/ui/TodoListApp.kt
@@ -1,21 +1,41 @@
 package com.example.composetodolist.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.composetodolist.ui.screen.EditScreen
 import com.example.composetodolist.ui.screen.MainScreen
 
 @Composable
 fun TodoListApp() {
     val navController = rememberNavController()
 
-    NavHost(
-        navController = navController,
-        startDestination = Screen.Main.route
-    ) {
-        composable(Screen.Main.route) {
-            MainScreen()
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { navController.navigate(Screen.Edit.route) }) {
+                Icon(Icons.Filled.Edit, contentDescription = "Edit")
+            }
+        }
+    ) { paddingValues ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Main.route,
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            composable(Screen.Main.route) {
+                MainScreen()
+            }
+            composable(Screen.Edit.route) {
+                EditScreen()
+            }
         }
     }
 }
