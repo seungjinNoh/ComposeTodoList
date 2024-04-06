4f7c815b99e2f6027b9be8fd25c034632210a433
seungjin <seungjin3047@gmail.com>
2024-04-06 17:20:51 +0900

    feat: EditScreen UI 구성
    - 입력창2, 이미지 버튼 1개 추가

--- app/src/main/java/com/example/composetodolist/ui/screen/EditScreen.kt
+++ app/src/main/java/com/example/composetodolist/ui/screen/EditScreen.kt
@@ -1,8 +1,69 @@
 package com.example.composetodolist.ui.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun EditScreen() {
 
+    var titleText by remember { mutableStateOf(TextFieldValue("")) }
+    var descriptionText by remember { mutableStateOf(TextFieldValue("")) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = titleText,
+                placeholder = { Text("제목을 입력하세요.") },
+                onValueChange = { titleText = it },
+            )
+
+            IconButton(
+                onClick = { /*TODO*/ },
+                modifier = Modifier.align(Alignment.CenterVertically)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = "저장",
+                    tint = Color.Green
+                )
+            }
+        }
+        
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(0.7f),
+            value = descriptionText,
+            onValueChange = { descriptionText = it },
+            placeholder = { Text(text = "내용을 입력하세요.") }
+        )
+        Spacer(modifier = Modifier.weight(0.3f))
+    }
 }
