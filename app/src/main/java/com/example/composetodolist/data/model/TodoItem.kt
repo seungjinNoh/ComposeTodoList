0fbe2c758cb5f69bcf4f2336a1180f2f357a03ed
seungjin <seungjin3047@gmail.com>
2024-04-09 23:11:36 +0900

    refactor: TodoItem id 값 추가
    - 구분값 id(UUID) 추가

--- app/src/main/java/com/example/composetodolist/data/model/TodoItem.kt
+++ app/src/main/java/com/example/composetodolist/data/model/TodoItem.kt
@@ -1,6 +1,9 @@
 package com.example.composetodolist.data.model
 
+import java.util.UUID
+
 data class TodoItem(
+    val id: UUID = UUID.randomUUID(), // 고유 식별자 추가
     val title: String,
     val description: String,
     var isComplete: Boolean
