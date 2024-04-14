fd262d4e60642c064c1c45d96a37fe928b122d20
seungjin <seungjin3047@gmail.com>
2024-04-14 20:09:15 +0900

    feat: TodoItemEntity 생성
    - DB 저장용 Entity

---
+++ app/src/main/java/com/example/composetodolist/data/entity/TodoItemEntity.kt
@@ -0,0 +1,14 @@
+package com.example.composetodolist.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "todo_items")
+data class TodoItemEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: UUID = UUID.randomUUID(),
+    val title: String,
+    val description: String,
+    var isComplete: Boolean
+)
