d405f8569d8ca19527b2e3c91c10eb5ccf07dd8c
seungjin <seungjin3047@gmail.com>
2024-04-15 23:14:46 +0900

    feat: mapper 메소드 추가
    - Entity <-> TodoItem 메소드

---
+++ app/src/main/java/com/example/composetodolist/data/DataMapper.kt
@@ -0,0 +1,22 @@
+package com.example.composetodolist.data
+
+import com.example.composetodolist.data.entity.TodoItemEntity
+import com.example.composetodolist.data.model.TodoItem
+
+fun mapperEntityToItem(todoItemEntity: TodoItemEntity): TodoItem {
+    return TodoItem(
+        id = todoItemEntity.id,
+        title = todoItemEntity.title,
+        description = todoItemEntity.description,
+        isComplete = todoItemEntity.isComplete
+    )
+}
+
+fun mapperItemToEntity(todoItem: TodoItem): TodoItemEntity {
+    return TodoItemEntity(
+        id = todoItem.id,
+        title = todoItem.title,
+        description = todoItem.description,
+        isComplete = todoItem.isComplete
+    )
+}
