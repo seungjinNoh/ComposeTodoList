f767b6002d50013fbade047fb4b45972578cfda7
seungjin <seungjin3047@gmail.com>
2024-04-16 23:00:43 +0900

    feat: GETQuery 어노테이션 추가

--- app/src/main/java/com/example/composetodolist/data/dao/TodoItemDao.kt
+++ app/src/main/java/com/example/composetodolist/data/dao/TodoItemDao.kt
@@ -3,12 +3,14 @@ package com.example.composetodolist.data.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.composetodolist.data.entity.TodoItemEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface TodoItemDao {
 
+    @Query("SELECT * FROM todo_items")
     fun getAllTodoItems(): Flow<List<TodoItemEntity>>
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertOrUpdateTodoItem(todoItemEntity: TodoItemEntity)
