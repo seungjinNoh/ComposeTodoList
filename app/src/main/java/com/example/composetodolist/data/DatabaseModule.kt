4702dd7a883b540298791e2beb30692f2d54f786
seungjin <seungjin3047@gmail.com>
2024-04-16 22:44:44 +0900

    feat: DB di 클래스 수정
    - companion object 밖으로 빼내기

--- app/src/main/java/com/example/composetodolist/data/DatabaseModule.kt
+++ app/src/main/java/com/example/composetodolist/data/DatabaseModule.kt
@@ -9,29 +9,33 @@ import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class DatabaseModule {
+object DatabaseModule {
 
-    companion object {
-        @Provides
-        @Singleton
-        fun provideDatabase(context: Context): TodoDatabase {
-            return Room.databaseBuilder(
-                context = context.applicationContext,
-                TodoDatabase::class.java,
-                "todo_database"
-            ).build()
-        }
-
-        @Provides
-        fun provideTodoItemDao(database: TodoDatabase): TodoItemDao {
-            return database.todoDao()
-        }
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext context: Context): TodoDatabase {
+        return Room.databaseBuilder(
+            context.applicationContext,
+            TodoDatabase::class.java,
+            "todo_database"
+        ).build()
     }
+
+    @Provides
+    fun provideTodoItemDao(database: TodoDatabase): TodoItemDao {
+        return database.todoDao()
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DatabaseProvider {
 
     @Binds
     abstract fun bindTodoItemRepository(
